34065cced129d473d9d1751c0540bceca92f7076
FABIANE DOS REIS ANCAI <fabiane.dos@cs.up.edu.br>
2025-05-05 21:10:43 -0300

    Primeiro commit

---
+++ JogoAdivinhacao/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class JogoAdivinhacao
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int numeroSecreto = random.Next(1, 101); // Número entre 1 e 100
+        int tentativa = 0;
+        int chute;
+
+        Console.WriteLine("Tente adivinhar o número entre 1 e 100!");
+
+        do
+        {
+            Console.Write("Digite seu palpite: ");
+            bool valido = int.TryParse(Console.ReadLine(), out chute);
+
+            if (!valido || chute < 1 || chute > 100)
+            {
+                Console.WriteLine("Por favor, digite um número válido entre 1 e 100.");
+                continue;
+            }
+
+            tentativa++;
+
+            if (chute < numeroSecreto)
+            {
+                Console.WriteLine("O número secreto é MAIOR.");
+            }
+            else if (chute > numeroSecreto)
+            {
+                Console.WriteLine("O número secreto é MENOR.");
+            }
+            else
+            {
+                Console.WriteLine($"Parabéns! Você acertou em {tentativa} tentativas.");
+            }
+
+        } while (chute != numeroSecreto);
+    }
+}
+
---
+++ ValidadorSenha/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class ValidadorSenha
+{
+    static void Main()
+    {
+        Console.Write("Digite uma senha: ");
+        string senha = Console.ReadLine();
+
+        if (ValidarSenha(senha))
+        {
+            Console.WriteLine("Senha forte!");
+        }
+        else
+        {
+            Console.WriteLine("Senha fraca. A senha deve conter no mínimo:");
+            Console.WriteLine("- 8 caracteres");
+            Console.WriteLine("- 1 letra maiúscula");
+            Console.WriteLine("- 1 número");
+            Console.WriteLine("- 1 caractere especial (ex: !, @, #)");
+        }
+    }
+
+    static bool ValidarSenha(string senha)
+    {
+        if (senha.Length < 8)
+            return false;
+
+        bool temMaiuscula = false;
+        bool temNumero = false;
+        bool temEspecial = false;
+
+        foreach (char c in senha)
+        {
+            if (char.IsUpper(c))
+                temMaiuscula = true;
+            else if (char.IsDigit(c))
+                temNumero = true;
+            else if (!char.IsLetterOrDigit(c))
+                temEspecial = true;
+        }
+
+        return temMaiuscula && temNumero && temEspecial;
+    }
+}
+
